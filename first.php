3e78317fd60bfebf4af128e765a3b1c675d5d5c6
DJ <douglass.dj@gmail.com>
2018-08-28 17:35:28 -0500

    Commit message

---
+++ first.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title? Basic PHP Page </head>
+</html>
+
+
+<?php
+?>
+<body>
+	<!-- Script 1.2 - firrst.php -->
+	<p> This is standard HTML</p>
+</body>
+</html>
+
