4ea7577236c47ed002d99ffa1dc2322936c7fa3e
DJ <douglass.dj@gmail.com>
2018-08-29 22:54:52 -0500

    chapter 1.6

---
+++ strings.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> PreDefined Varible </title>
+</html>
+
+
+<?php # Script 1.6 - strings.php
+
+	// Create varibles
+	
+$first_name = 'DJ';
+$last_name = 'Douglass';
+$book = 'Matt\'s Story'; 
+
+
+
+// Print the values:
+echo "<p>The book <em>$book</em> was written by $first_name $last_name.</p>";
+
+?>
+<body>
+
+
+
+</body>
+</html>
