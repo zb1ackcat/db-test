2e91a9b17b45e132b0940d4f66f9c5a41f826cd0
DJ <douglass.dj@gmail.com>
2018-08-31 21:34:11 -0500

    numbers update

--- numbers.php
+++ numbers.php
@@ -9,9 +9,25 @@
 
 	//Varibles that are currently not varible. 
 
-	$quantity = 30;
-	$price = 119.95;
+	$quantity = 30022;
+	$price = 119.11;
 	$taxrate = .05;
+
+	// The mathes go here
+	
+	$total = $quantity * $price;
+	$total = $total * $taxrate;
+	$total = number_format($total, 2);
+
+	//The printed HTMML
+	
+	echo '<p>You are purchasing 
+	<strong>' . $quantity . 
+	'</strong> widgets(s) at a cost
+	of <strong>$' . $price . 
+	'</strong> each. With tax, the
+	total comes to <strong>$' . 
+	$total . '</strong>.</p>';
 
 ?>
 <body>
