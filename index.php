8681f5dced9fe8d0934d2e0adb80e7c973ab86ff
DJ <douglass.dj@gmail.com>
2018-09-03 18:17:06 -0500

    3.4

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php # script 3.4 - index.php
+	$page_title = 'Welcome to this Site!';
+	include('includes/header.html')
+	
+?>
+
+<div class="page-header">
+	<h1>Content Header</h1>
+	<p> Stuff and things go here.</p>
+	
+<?php 
+	include('includes/footer.html');
+	?>
