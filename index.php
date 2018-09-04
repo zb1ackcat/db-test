6ead297b5f8db7d8485c14a773fd437755ef3cfe
DJ <douglass.dj@gmail.com>
2018-09-04 08:08:09 -0500

    3.6

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php # script 3.4 - index.php
+<?php # script 3.6 - index.php
 	$page_title = 'Welcome to this Site!';
-	include('includes/header.html')
+	include('includes/header.html');
+	
+	
+	### AD ####
+	
+	function create_ad() {
+  echo '<div class="alert alert-info" role="alert"><p>This is an annoying ad! This is an annoying ad! This is an annoying ad! This is an annoying ad!</p></div>';
+}
+
+	#### PRINT AD ####
+
+create_ad();
 	
 ?>
 
@@ -8,6 +19,6 @@
 	<h1>Content Header</h1>
 	<p> Stuff and things go here.</p>
 	
-<?php 
-	include('includes/footer.html');
-	?>
+<?php create_ad(); ?>
+	
+<?php include('includes/footer.html');?>
